4ec4644e3ea26f7c3f6993c10daa57afef5141ab
Lian1998 <l1ian1998hyhy@gmail.com>
2024-02-12 00:28:06 +0800

    测试 hooks

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -14,44 +14,20 @@ module.exports = {
         "subject-case": ["off", "never"],
         "body-leading-blank": ["warn", "always"],
         "footer-leading-blank": ["warn", "always"],
-        "header-max-length": ["error", "always", 108], // header上最大108字符
-        "type-enum": [
-            "error",
-            "always",
+        "header-max-length": ["error", "always", 108],
+        "type-enum": ["error", "always",
             [
-
-                // 新增特性
-                "feat",
-
-                // 修补代码
-                "fix",
-
-                // 性能优化
-                "perf",
-
-                // 其他修改
-                "chore",
-
-                // 文档注释
-                "docs",
-
-                // 回退版本
-                "revert",
-
-                // 代码重构
-                "refactor",
-
-                // 测试用例
-                "test",
-
-                // 线上热修
-                "hotfix",
-
-                // 构建管线
-                "build",
-
-                // 持续集成
-                "ci",
+                "feat",     // 新增特性
+                "fix",      // 修补代码
+                "perf",     // 性能优化
+                "chore",    // 其他修改
+                "docs",     // 文档注释
+                "revert",   // 回退版本
+                "refactor", // 代码重构
+                "test",     // 测试用例
+                "hotfix",   // 线上热修
+                "build",    // 构建管线
+                "ci",       // 持续集成
             ],
         ],
     },
