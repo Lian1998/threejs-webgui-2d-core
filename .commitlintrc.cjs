ddb074c769a064e44eec6fc80e221eafaccabcac
Lian1998 <l1ian1998hyhy@gmail.com>
2024-02-12 19:50:14 +0800

    ci(githooks): 更新githooks各项规则
    ci(githooks): 更新commitlint规则
    
    测试一下提交的body内容

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,36 +1,19 @@
-// commitlint 等级
 // 关闭: "off"      0
 // 警告: "warn"     1
 // 错误: "error"    2
 
+// 新增特性 "feat"
+// 修补代码 "fix"
+// 性能优化 "perf"
+// 其他修改 "chore"
+// 文档注释 "docs"
+// 回退版本 "revert"
+// 代码重构 "refactor"
+// 测试用例 "test"
+// 线上热修 "hotfix"
+// 构建管线 "build"
+// 持续集成 "ci"
+
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-case": ["off"],
-        "type-empty": ["off"],
-        "scope-empty": ["off"],
-        "scope-case": ["off"],
-        "subject-full-stop": ["off", "never"],
-        "subject-case": ["off", "never"],
-        "body-leading-blank": ["warn", "always"],
-        "footer-leading-blank": ["warn", "always"],
-        "header-max-length": ["error", "always", 108],
-        "type-enum": [
-            "error",
-            "always",
-            [
-                "feat",     // 新增特性
-                "fix",      // 修补代码
-                "perf",     // 性能优化
-                "chore",    // 其他修改
-                "docs",     // 文档注释
-                "revert",   // 回退版本
-                "refactor", // 代码重构
-                "test",     // 测试用例
-                "hotfix",   // 线上热修
-                "build",    // 构建管线
-                "ci",       // 持续集成
-            ],
-        ],
-    },
+    extends: ["@commitlint/config-conventional"]
 };
