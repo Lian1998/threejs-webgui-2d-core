eb7dda41e7e54a78165c3f066ce5261eecc8b352
Lian1998 <l1ian1998hyhy@gmail.com>
2024-10-03 21:19:13 +0800

    build: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import vuePlugin from "@vitejs/plugin-vue";
+import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,8 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
-export default defineConfig(({command, mode}) => {
-    
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, "./.env/"), ""); // 加载额外的env环境文件
 
   return {
@@ -27,23 +26,25 @@ export default defineConfig(({command, mode}) => {
     // https://vitejs.dev/config/shared-options.html#assetsinclude
     // https://vitejs.dev/guide/assets.html
     // https://github.com/vitejs/vite/blob/65ef38375045935d12ad3f4dd6da90ab5860b5cd/packages/vite/src/node/constants.ts#L98
-    assetsInclude: [ 
-        /\.(vs|fs|vert|frag|glsl|shader)$/i, // 使得shader程序可以?raw支持以原生字符串导入
-    ], 
+    assetsInclude: [
+      /\.(vs|fs|vert|frag|glsl|shader)$/i, // 使得shader程序可以?raw支持以原生字符串导入
+    ],
 
     resolve: {
       alias: {
         "@source": path.resolve(__dirname, "./source"),
         "@assets": path.resolve(__dirname, "./assets"),
         "@libs": path.resolve(__dirname, "./libs"),
-        'three': path.resolve(__dirname, './libs/three.js-r161/src/Three.js'),
-        'three_addons': path.resolve(__dirname, './libs/three.js-r161/examples/jsm'),
-      }
+        three: path.resolve(__dirname, "./libs/three.js-r161/src/Three.js"),
+        three_addons: path.resolve(__dirname, "./libs/three.js-r161/examples/jsm"),
+        "gl-matrix": path.resolve(__dirname, "./libs/toji-gl-matrix-v3.3.0-73-g02bb0b7/toji-gl-matrix-02bb0b7/dist"),
+        "@WebGL2YT": path.resolve(__dirname, "./source/WebGL2-YouTube"),
+      },
     },
-    
+
     plugins: [
-      vue(),                      // Vue3 SFC(Single-File Component) 解析器
-      vueJsx(),                   // Vue3 SFC 支持 JSX/TSX 解析器
+      vuePlugin(),                  // Vue3 SFC(Single-File Component) 解析器
+      vueJsxPlugin(),               // Vue3 SFC 支持 JSX/TSX 解析器
     ],
 
     server: {
@@ -56,36 +57,36 @@ export default defineConfig(({command, mode}) => {
         "/test_server1": {
           target: "http://test.server1.com",
           changeOrigin: true,
-          rewrite: (path) => { return path.replace(/^\/test_server1/, "/apis"); }
-        }
-      }
-    },
-
-    build: {
-      target: "modules", //  native ES Modules, native ESM dynamic import, import.meta
-      minify: "esbuild",
-      sourcemap: true,
-      modulePreload: {
-        polyfill: true, // 自动注入pollyfill
-      },
-      chunkSizeWarningLimit: 1024,                        // 块大小(单位kB)(主要影响javascript包下载/CPU执行时间)
-      assetsDir: "assets",                                // 静态的资产(打包模式作为静态资产处理, 并将结果直接输出到public/assets)
-      assetsInlineLimit: 10240,                           // 静态资源如果小于 10mB 会被解析成 base64
-      outDir: path.resolve(__dirname, "./dist/"),         // 打包输出路径
-      emptyOutDir: true,                                  // 打包清空目录
-      rollupOptions: {
-        // https://vitejs.dev/guide/build.html#multi-page-app
-        input: {
-          "index": path.resolve(__dirname, "./multi-pages/index.html"),
-          "tsx": path.resolve(__dirname, "./multi-pages/tsx/index.html"),
-          "threejs": path.resolve(__dirname, "./multi-pages/threejs/index.html")
+          rewrite: (path) => { return path.replace(/^\/test_server1/, "/apis"); },
         },
-        output: {
-          // https://vitejs.dev/config/build-options.html#build-rollupoptions
-          // https://rollupjs.org/configuration-options/#output-manualchunks
-          manualChunks: { lodash: ["@libs/lodash"] }
-        }
+      },
+
+      build: {
+        target: "modules", //  native ES Modules, native ESM dynamic import, import.meta
+        minify: "esbuild",
+        sourcemap: true,
+        modulePreload: {
+          polyfill: true, // 自动注入pollyfill
+        },
+        chunkSizeWarningLimit: 1024, // 块大小(单位kB)(主要影响javascript包下载/CPU执行时间)
+        assetsDir: "assets", // 静态的资产(打包模式作为静态资产处理, 并将结果直接输出到public/assets)
+        assetsInlineLimit: 10240, // 静态资源如果小于 10mB 会被解析成 base64
+        outDir: path.resolve(__dirname, "./dist/"), // 打包输出路径
+        emptyOutDir: true, // 打包清空目录
+        rollupOptions: {
+          // https://vitejs.dev/guide/build.html#multi-page-app
+          input: {
+            "index": path.resolve(__dirname, "./multi-pages/index.html"),
+            "tsx": path.resolve(__dirname, "./multi-pages/tsx/index.html"),
+            "threejs": path.resolve(__dirname, "./multi-pages/threejs/index.html"),
+          },
+          output: {
+            // https://vitejs.dev/config/build-options.html#build-rollupoptions
+            // https://rollupjs.org/configuration-options/#output-manualchunks
+            manualChunks: { lodash: ["@libs/lodash"] },
+          },
+        },
       },
     },
-  }
+  };
 });
