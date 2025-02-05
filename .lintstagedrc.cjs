68b2c4241b938c5b28c349425d973ff47f20fc7f
Lian1998 <l1ian1998hyhy@gmail.com>
2025-02-06 00:04:14 +0800

    fix: lintstaged configuration

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,12 +1,3 @@
 module.exports = {
-
-    "*.{js,jsx,vue,ts,tsx,json,css,scss,sass,less,html}": async (fileAbsPaths) => {
-        
-        const commandsRes = []; // 存储指令集
-
-        // 自动格式化文件
-        commandsRes.push(`npx prettier --write`);
-        
-        return commandsRes;
-    }
+  "*.{js,jsx,vue,ts,tsx,css,scss,sass,less,html}": [ "prettier --write" ]
 }
