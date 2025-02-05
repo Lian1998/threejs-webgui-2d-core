88c6069d23e0afa57bd517662aa0ea580cacc28e
Lian1998 <l1ian1998hyhy@gmail.com>
2025-02-05 17:04:40 +0000

    update .env/vite-env.d.ts.
    Signed-off-by: Lian1998 <l1ian1998hyhy@gmail.com>

--- .env/vite-env.d.ts
+++ .env/vite-env.d.ts
@@ -1,31 +1,31 @@
 /// <reference types="vite/client" />
 
-declare module '.vs' {
+declare module '*.vs' {
     const value: string
     export default value
 }
 
-declare module '.fs' {
+declare module '*.fs' {
     const value: string
     export default value
 }
 
-declare module '.vert' {
+declare module '*.vert' {
     const value: string
     export default value
 }
 
-declare module '.frag' {
+declare module '*.frag' {
     const value: string
     export default value
 }
 
-declare module '.glsl' {
+declare module '*.glsl' {
     const value: string
     export default value
 }
 
-declare module '.shader' {
+declare module '*.shader' {
     const value: string
     export default value
 }
