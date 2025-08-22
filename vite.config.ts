37c959f84ec3c21a3246244f2ef08574bb695d4c
Lian1998 <l1ian1998hyhy@gmail.com>
2025-08-22 15:26:30 +0800

    chore: bam!

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,17 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const _dir = (relativePath: string) => path.resolve(__dirname, relativePath)
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, path.resolve(__dirname, "./.env/"), ""); // 加载额外的env环境文件
+  const env = loadEnv(mode, _dir("./.env/"), ""); // 加载额外的env环境文件
 
   return {
-    appType: "mpa",
-    root: path.resolve(__dirname, "./multi-pages/"), // 多页面打包模式更改入口
-    envDir: path.resolve(__dirname, "./.env/"),
+    root: _dir("./multi-pages/"), // 多页面打包模式更改入口
+    envDir: _dir( "./.env/"),
     envPrefix: "CLIENT_",
-    publicDir: path.resolve(__dirname, "./public/"),
+    publicDir: _dir("./public/"),
     clearScreen: true,
 
     // 以 `micromatch/picomatch` 规则匹配到的`import`语句 https://github.com/micromatch/picomatch#globbing-features
@@ -32,11 +32,9 @@ export default defineConfig(({ command, mode }) => {
 
     resolve: {
       alias: {
-        "@source": path.resolve(__dirname, "./source"),
-        "@assets": path.resolve(__dirname, "./assets"),
-        "@libs": path.resolve(__dirname, "./libs"),
-        "three": path.resolve(__dirname, "./libs/three.js-r170/src/Three.js"),
-        "three_addons": path.resolve(__dirname, "./libs/three.js-r170/examples/jsm"),
+        "@source": _dir("./source"),
+        "@assets": _dir("./assets"),
+        "@libs": _dir("./libs"),
       },
     },
 
@@ -46,44 +44,24 @@ export default defineConfig(({ command, mode }) => {
     ],
 
     server: {
-      host: "0.0.0.0",            // 广播ip地址
-      port: 5173,
+      port: 13261,
       strictPort: true,           // 固定端口(不会由于占用而自动顺延端口)
-      open: false,                // 不自动打开页面
-      cors: true,                 // 打开跨域请求
-      proxy: {                    // 代理配置
-        "/test_server1": {
-          target: "http://test.server1.com",
-          changeOrigin: true,
-          rewrite: (path) => { return path.replace(/^\/test_server1/, "/apis"); },
-        },
-      },
 
       build: {
         target: "modules", //  native ES Modules, native ESM dynamic import, import.meta
         minify: "esbuild",
         sourcemap: true,
-        modulePreload: {
-          polyfill: true, // 自动注入pollyfill
-        },
         chunkSizeWarningLimit: 1024, // 块大小(单位kB)(主要影响javascript包下载/CPU执行时间)
         assetsDir: "assets", // 静态的资产(打包模式作为静态资产处理, 并将结果直接输出到public/assets)
         assetsInlineLimit: 10240, // 静态资源如果小于 10mB 会被解析成 base64
-        outDir: path.resolve(__dirname, "./dist/"), // 打包输出路径
+        outDir: _dir("./dist/"), // 打包输出路径
         emptyOutDir: true, // 打包清空目录
-        rollupOptions: {
-          // https://vitejs.dev/guide/build.html#multi-page-app
-          input: {
-            "index": path.resolve(__dirname, "./multi-pages/index.html"),
-            "tsx": path.resolve(__dirname, "./multi-pages/tsx/index.html"),
-            "threejs": path.resolve(__dirname, "./multi-pages/threejs/index.html"),
-          },
-          output: {
-            // https://vitejs.dev/config/build-options.html#build-rollupoptions
-            // https://rollupjs.org/configuration-options/#output-manualchunks
-            manualChunks: { lodash: ["@libs/lodash"] },
-          },
-        },
+        rollupOptions: { external: [], output: {}, },
+        lib: {
+          entry: _dir("./source/main.ts"),
+          name: "webgui2d",
+          formats: ["es", "cjs", "iife"], // 输出格式
+        }
       },
     },
   };
