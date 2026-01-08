ed8311c780bd8a7c2a43f36fec8dd6cb2ec04a01
钮宏毅(Lian1998) <l1ian1998hyhy@gmail.com>
2026-01-08 17:28:36 +0800

    feat: 视口调整代码

--- core/GeoCollectionLoader/debug/examples/dachanwan.ts
+++ core/GeoCollectionLoader/debug/examples/dachanwan.ts
@@ -1,3 +1,9 @@
+///////////////////////////////////// 底图配置 /////////////////////////////////////
+const MAP_CENTER = [520, 285];
+const MAP_VIEW_SIZE = 300;
+
+///////////////////////////////////// 公共文件 //////////////////////////////////////
+
 import "normalize.css";
 import * as THREE from "three";
 
@@ -13,22 +19,30 @@ renderer.setClearColor(0xffffff, 0.0);
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 viewport.appendChild(renderer.domElement);
 
-import { MapControls } from "three_addons/controls/MapControls.js";
-const viewSize = 600;
+const viewSize = MAP_VIEW_SIZE;
 const camera = new THREE.OrthographicCamera(-viewSize * aspect, viewSize * aspect, viewSize, -viewSize, 0.1, 5000);
-const center = new THREE.Vector3(0, 0, 0);
+viewportResizeDispatcher.addResizeEventListener(({ message: { width, height } }) => {
+  const aspect = width / height;
+  camera.left = -viewSize * aspect;
+  camera.right = viewSize * aspect;
+  camera.top = viewSize;
+  camera.bottom = -viewSize;
+  camera.updateProjectionMatrix();
+});
 
-camera.position.set(center.x, 1000, center.z);
-camera.up.set(0, 1, 0);
-camera.lookAt(center);
-camera.zoom = 1;
-camera.updateProjectionMatrix();
-
+import { MapControls } from "three_addons/controls/MapControls.js";
+const center = new THREE.Vector3(MAP_CENTER[0], 0, MAP_CENTER[1]);
 const controls = new MapControls(camera, renderer.domElement);
+controls.maxPolarAngle = Math.PI / 2;
 controls.minZoom = 0.5;
 controls.maxZoom = 5;
 controls.target.copy(center);
 controls.update();
+controls.saveState();
+camera.position.set(center.x, 1000, center.z);
+camera.up.set(0, 1, 0);
+camera.zoom = 1;
+camera.updateProjectionMatrix();
 
 const scene = new THREE.Scene();
 
@@ -36,6 +50,7 @@ const scene = new THREE.Scene();
 import type { FeatureCollection } from "geojson";
 import type { LineString } from "geojson";
 
+import type { PointsRepresentation } from "@core/GeoCollectionLoader/mesh-line/";
 import { MeshLineGeometry } from "@core/GeoCollectionLoader/mesh-line/";
 import { MeshLineMaterial } from "@core/GeoCollectionLoader/mesh-line/";
 import { MeshLineMaterialParameters } from "@core/GeoCollectionLoader/mesh-line/";
@@ -50,23 +65,11 @@ scene.add(group0);
   const handleMapShaperFile = (data: FeatureCollection<LineString>, materialConfiguration: MeshLineMaterialParameters) => {
     switch (data.type) {
       case "FeatureCollection": {
-        // const coordinatesCollection = [];
-        // for (let i = 0; i < data.features.length; i++) {
-        //   const feature = data.features[i];
-        //   const coordinates = feature.geometry.coordinates;
-        //   coordinatesCollection.push(coordinates);
-        // }
-        // const mlGeometry = new MeshLineGeometry();
-        // mlGeometry.setMultiLines(coordinatesCollection);
-        // const mlMaterial = new MeshLineMaterial(materialConfiguration);
-        // const mesh = new THREE.Mesh(mlGeometry, mlMaterial);
-        // mesh.frustumCulled = false;
-        // group0.add(mesh);
         for (let i = 0; i < data.features.length; i++) {
           const feature = data.features[i];
           const coordinates = feature.geometry.coordinates;
           const mlGeometry = new MeshLineGeometry();
-          mlGeometry.setPoints(coordinates);
+          mlGeometry.setPoints(coordinates as PointsRepresentation);
           const mlMaterial = new MeshLineMaterial(materialConfiguration);
           const mesh = new THREE.Mesh(mlGeometry, mlMaterial);
           mesh.frustumCulled = false;
@@ -116,9 +119,24 @@ scene.add(group0);
   ]).finally(() => group0.traverse((object3D) => object3D.layers.set(0)));
 }
 
+//////////////////////////////////////// 坐标轴助手 ////////////////////////////////////////
+
 import { AxesHelper } from "three";
-const ah = new AxesHelper(1000);
-scene.add(ah);
+const axesHepler = new AxesHelper(1000);
+scene.add(axesHepler);
+
+//////////////////////////////////////// 坐标定位 ////////////////////////////////////////
+
+import { getXZPosition } from "@source/utils/pointerCoordinates";
+{
+  const coordinatesEl = document.querySelector("#coordinates");
+  ViewportResizeDispatcher.getClassInstance<ViewportResizeDispatcher>("default").viewportElement.addEventListener("mousemove", (e) => {
+    const pos = getXZPosition(e, camera, renderer);
+    coordinatesEl.innerHTML = `${pos.x.toFixed(2)}, ${pos.z.toFixed(2)}`;
+  });
+}
+
+//////////////////////////////////////// 渲染循环 ////////////////////////////////////////
 
 const clock = new THREE.Clock();
 
